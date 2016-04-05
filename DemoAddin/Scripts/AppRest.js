b7a2a355ac6b892ae75258290b8863908310f9f2
Luis Felipe Gil <lamaslg@gmail.com>
2016-04-05 10:45:04 +0000

    a

--- DemoAddin/Scripts/AppRest.js
+++ DemoAddin/Scripts/AppRest.js
@@ -20,3 +20,38 @@
     });
 
 }
+var guardarItem = function () {
+    var url = _spPageContextInfo.webServerRelativeUrl + "/_api/web/lists/getByTitle('Personas')/items";
+    var digest = $("#__REQUESTDIGEST").val();
+
+    var obj = {
+        '__metadata': { 'type': 'SP.Data.PersonasListItem' },
+        'Nombre': $("#input-nombre").val(),
+        'Edad': parseInt($("#input-edad").val())
+    };
+
+    $.ajax({
+        url: url,
+        type: "POST",
+        data: JSON.stringify(obj),
+        headers: {
+            'accept': 'application/json;odata=verbose',
+            'content-type': 'application/json;odata=verbose',
+            'X-RequestDigest':digest
+        },
+        success: function () {
+            getLista();
+        },
+        error: function (err) {
+            alert(JSON.stringify(err));
+        }
+    });
+
+}
+
+$(document).ready(function () {
+    
+    $("#button-add").click(guardarItem);
+    getLista();
+
+});
